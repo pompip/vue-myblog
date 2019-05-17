57f532fec7aa2ce260b68e4b14bf633505f8f6c4
rodon557 <546492510@qq.com>
2019-05-17 23:13:19 +0800

    vue-route

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,32 @@
 <template>
   <div id="app">
-    <Nav />
+    <Nav/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>
+      <router-link to="/foo">Go to Foo</router-link>
+      <router-link to="/bar">Go to Bar</router-link>
+    </p>
+
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Nav from './components/Nav.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import Nav from "./components/Nav.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld,Nav
+    HelloWorld,
+    Nav
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
