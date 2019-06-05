ad6c629e7e3bddfd9eb6021513cd203848b903ec
rodon557 <546492510@qq.com>
2019-06-05 18:20:16 +0800

    http token

--- src/api/Http.js
+++ src/api/Http.js
@@ -1,17 +1,26 @@
 import axios from 'axios'
 import Qs from 'qs'
-// import Vue from "vue";
-import {Store} from '@/store' 
+import store from '../store/index'
 
-export default  axios.create({
+const a = axios.create({
     baseURL: "http://java.asuscomm.com:9000/api",
-    headers:{
-        "token":Store.Login.state.token
-    },
     timeout: 5000,
     transformRequest: [function (data) {
         // 对 data 进行任意转换处理
+        console.log(store)
         return Qs.stringify(data)
     }]
-    
-})
+
+});
+a.interceptors.request.use(
+    function (config) {
+        let t = store.state.Login.token;
+        if (t) {
+            config.headers = {
+                token: store.state.Login.token
+            }
+        }
+        return config;
+    }
+)
+export default a;
