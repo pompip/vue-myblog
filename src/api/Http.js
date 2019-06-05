efb88196813b174be6700035d5841cee0728dc46
pompip <pompip@163.com>
2019-06-05 20:59:20 +0800

    http

--- src/api/Http.js
+++ src/api/Http.js
@@ -3,11 +3,24 @@ import Qs from 'qs'
 // import Vue from "vue";
 import {Store} from '@/store' 
 
+axios.interceptors.request.use(
+    config => {
+        if (token) {  // 每次发送请求之前判断是否存在token，如果存在，则统一在http请求的header都加上token，不用每次请求都手动添加了
+            config.headers.Authorization = token;
+        }
+        return config;
+    },
+    err => {
+        return Promise.reject(err);
+
+    });
+
 export default  axios.create({
     baseURL: "http://java.asuscomm.com:9000/api",
     headers:{
-        "token":Store.Login.state.token
+        "token":"123"
     },
+    changeOrigin:true,
     timeout: 5000,
     transformRequest: [function (data) {
         // 对 data 进行任意转换处理
