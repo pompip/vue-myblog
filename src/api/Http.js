708185a84b102a7d0e5547f046f664b8029a7e32
liukechong <liukc@bonree.com>
2019-10-14 17:37:59 +0800

    token

--- src/api/Http.js
+++ src/api/Http.js
@@ -4,18 +4,11 @@ import store from '../store/index'
 
 const a = axios.create({
     baseURL: "http://java.asuscomm.com:9000/api",
-    timeout: 5000,
-    // transformRequest: [function (data) {
-    //     // 对 data 进行任意转换处理
-    //     console.log(store)
-    //     return Qs.stringify(data)
-    // }]
-
+    timeout: 5000
 });
 a.interceptors.request.use(
     function (config) {
         let t = store.state.Login.token;
-        console.log("token:"+t);
         if (t) {
             config.headers = {
                 token: store.state.Login.token
